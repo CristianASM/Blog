f264b6c24c224d48464fe23ef4a645c4886581a4
Cristian Soto <cristiansoto.m10@gmail.com>
2023-11-03 17:44:02 -0300

    Agregue edicion y eliminar post

--- src/main/java/com/blog/Controller/PostController.java
+++ src/main/java/com/blog/Controller/PostController.java
@@ -49,4 +49,24 @@ public class PostController {
         model.addAttribute("post", post);
         return "viewPost";
     }
+    @GetMapping("/edit/{id}")
+    public String editPost(@PathVariable Long id, Model model){
+        Post postToEdit = postService.getPost(id);
+        model.addAttribute("edit", postToEdit);
+        return "editPost";
+    }
+    @PostMapping("/edit/{id}")
+    public String saveEdit(@PathVariable Long id, @ModelAttribute("edit") Post post){
+        Post postToSave = postService.getPost(id);
+        postToSave.setTitle(post.getTitle());
+        postToSave.setBody(post.getBody());
+        postToSave.setCreatedDate(LocalDateTime.now());
+        postService.updatePost(id, postToSave);
+        return "redirect:/";
+    }
+    @GetMapping("/delete/{id}")
+    public String deletePost(@PathVariable Long id){
+        postService.deletePost(id);
+        return "redirect:/";
+    }
 }
